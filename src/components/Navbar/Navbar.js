0e77c3005493051c08efccc42bc640a672c4ef0a
danduke2 <daniel.duke@publicissapient.com>
2023-09-15 14:25:05 -0400

    updates

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,10 +6,11 @@ const Navbar= () =>{
   <div className='navbar-container'>
     <div className='links-container'>
       <Link className='link-item' to="/">Home</Link>
-      <Link className='link-item' to="/contact">Contact</Link>
-      <Link className='link-item' to="/about">About</Link>
       <Link className='link-item' to="/photos">Photos</Link>
       <Link className='link-item' to="/engineering">Web Development</Link>
+      <Link className='link-item' to="/about">About</Link>
+      <Link className='link-item' to="/contact">Contact</Link>
+
     </div>
   </div>
   );
